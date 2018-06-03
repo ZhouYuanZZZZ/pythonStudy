3c928c579b19a5debb33e65443d2131166a73b1f
zy <1270157082@qq.com>
2018-06-04 01:51:15 +0800

    ...

---
+++ dataType/sequence.py
@@ -0,0 +1,50 @@
+print("gg" in "eggs")
+print('-----------------------------1---------------------------------')
+print('\n')
+
+lists = [[]] * 3
+print(lists)
+
+lists[0].append(3)
+lists[1].append(31)
+print(lists)
+print('-----------------------------2---------------------------------')
+print('\n')
+
+lists = [[] for i in range(3)]
+lists[0].append(3)
+lists[1].append(5)
+lists[2].append(7)
+print(lists)
+print('-----------------------------2---------------------------------')
+print('\n')
+
+lists = [1,2,3,4]
+lists.extend([33,44,9])
+lists.sort()
+print(lists)
+l1 = lists.index(44)
+print(l1)
+
+l2 = [1,55,4,4,7,8]
+del l2[0]
+print(l2)
+del l2[1:2]
+print(l2)
+l2.pop()
+print(l2)
+l2[0:2] = []
+print(l2)
+l2[1:4] = [11,22,33]
+print(l2)
+print('-----------------------------3---------------------------------')
+print('\n')
+
+r = range(10)
+print(r)
+t1 = tuple(r)
+print(t1)
+t2 = t1[1:]
+print(t2)
+print('-----------------------------4---------------------------------')
+print('\n')
---
+++ dataType/string.py
@@ -0,0 +1,24 @@
+s1 = "abcdefghijklmn"
+for item in s1:
+    print(item)
+
+print("--------------------")
+print(s1[0])
+print(s1[0:2])
+print(s1[:5])
+print(s1[2:])
+print(s1[:])
+print(s1[::2])
+print("--------------------")
+
+s2 = """ 4545 
+45456
+7878
+7878
+5154"""
+print(s2)
+
+print("-------------------")
+
+s3 = "X"
+print(s3 * 8)
