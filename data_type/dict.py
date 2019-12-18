03ac255b85a3b0e5120527f3e426b1525c746a67
zy1270157082@outlook.com <Zy0811150129>
2019-12-18 19:16:14 +0800

    dict

--- data_type/dict.py
+++ data_type/dict.py
@@ -1,66 +1,23 @@
-# Here is a small example using a dictionary:
-tel = {'jack': 4098, 'sape': 4139}
-tel['guido'] = 412777
 
-print(tel)
-print(tel['jack'])
+def create_dict():
+    a = dict(one=1, two=2, three=3)
+    b = {'one': 1, 'two': 2, 'three': 3}
+    c = dict(zip(['one', 'two', 'three'], [1, 2, 3]))
+    d = dict([('two', 2), ('one', 1), ('three', 3)])
+    e = dict({'three': 3, 'one': 1, 'two': 2})
 
-del tel['sape']
-tel['irv'] = 4127
-print(tel)
-
-print('--------------------1----------------------------')
-print('\n')
-
-# The dict() constructor builds dictionaries directly from sequences of key-value pairs:
-dict1 = dict([('sape', 4139), ('guido', 4127), ('jack', 4098)])
-print(dict1)
-print('--------------------2----------------------------')
-print('\n')
-
-# When looping through dictionaries, the key and corresponding value can be retrieved at the same time using the items() method.
-for k, v in dict1.items():
-    print(k+':'+str(v))
-print('--------------------3----------------------------')
-print('\n')
-
-a = dict(one=1, two=2, three=3)
-b = {'one': 1, 'two': 2, 'three': 3}
-c = dict(zip(['one', 'two', 'three'], [1, 2, 3]))
-d = dict([('two', 2), ('one', 1), ('three', 3)])
-e = dict({'three': 3, 'one': 1, 'two': 2})
-
-print(a == b == c == d == e)
-print('--------------------4----------------------------')
-print('\n')
-
-dishes = {'eggs': 2, 'sausage': 1, 'bacon': 1, 'spam': 500}
-keys = dishes.keys()
-values = dishes.values()
-
-# iteration
-n = 0
-for val in values:
-    n += val
-print(n)
+    print(a == b == c == d == e)
 
 
-# keys and values are iterated over in the same order
-list(keys)
-
-list(values)
-
-
-# view objects are dynamic and reflect dict changes
-del dishes['eggs']
-del dishes['sausage']
-list(keys)
+def iterate_map():
+    a = dict(one=1, two=2, three=3)
+    for (k, v) in a.items():
+        print("{}-{}".format(k, v))
 
 
-# set operations
-keys & {'eggs', 'bacon', 'salad'}
+iterate_map()
 
-keys ^ {'sausage', 'juice'}
 
-print('--------------------5----------------------------')
-print('\n')
+
+
+
